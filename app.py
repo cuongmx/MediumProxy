5e552f6c0e55676e807731a8b6473abc5400fdc6
cuongmx <cuongmx@gmail.com>
2021-01-13 21:13:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask('__main__')
 SITE_NAME = 'https://cuongmx.medium.com/'
-SITE_NEW = 'http://mediumx.herokuapp.com/'
+SITE_NEW = 'https://mediumx.herokuapp.com/'
 
 @app.route('/favicon.ico')
 #@app.route('/_/batch', methods=['POST'])
@@ -22,12 +22,12 @@ def proxy(path):
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
     headers.append(['Access-Control-Allow-Origin', '*'])
-    response = Response(resp.content.decode('utf-8').replace(SITE_NAME, "127.0.0.1"), resp.status_code, headers)
+    response = Response(resp.content.decode('utf-8').replace(SITE_NAME, SITE_NEW), resp.status_code, headers)
     return response
   elif request.method=='POST':
     resp = requests.post(f'{SITE_NAME}{path}',data=request.data)
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
     headers.append(['Access-Control-Allow-Origin', '*'])
-    response = Response(resp.content.decode('utf-8').replace(SITE_NAME, "127.0.0.1"), resp.status_code, headers)
+    response = Response(resp.content.decode('utf-8').replace(SITE_NAME, SITE_NEW), resp.status_code, headers)
     return response

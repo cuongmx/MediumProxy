71101e9650fabdcbfc2624aac370b61f53954b84
cuongmx <cuongmx@gmail.com>
2021-01-13 23:17:40 +0700

    update heroku button

--- app.py
+++ app.py
@@ -2,22 +2,21 @@ from flask import Flask,request,redirect,Response
 import requests, os, re
 
 app = Flask('__main__')
-SITE_NAME = os.environ.get('SITE_NAME', 'https://cuongmx.medium.com/')
-SITE_NEW = os.environ.get('SITE_NEW', 'https://m.cuong.mx/')
+MEDIUM_SITE = os.environ.get('MEDIUM_SITE', 'https://cuongmx.medium.com/')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['POST','GET', 'OPTION'])
 def proxy(path):
   if request.method=='GET':
-    resp = requests.get(f'{SITE_NAME}{path}')
+    resp = requests.get(f'{MEDIUM_SITE}{path}')
   elif request.method=='POST':
-    resp = requests.post(f'{SITE_NAME}{path}',data=request.data)
+    resp = requests.post(f'{MEDIUM_SITE}{path}',data=request.data)
   else: #'OPTION'
-    resp = requests.options(f'{SITE_NAME}{path}')
+    resp = requests.options(f'{MEDIUM_SITE}{path}')
   excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
   headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
   headers.append(['Access-Control-Allow-Origin', '*'])
-  resptext = resp.content.decode('utf-8').replace(SITE_NAME, SITE_NEW)
+  resptext = resp.content.decode('utf-8')
   resptext = resptext.replace("<head>","<head><script type=\"text/javascript\"> function removelisteners() { document.body.innerHTML = document.body.innerHTML; } window.onload = removelisteners; </script>")
   response = Response(resptext, resp.status_code, headers)
   return response

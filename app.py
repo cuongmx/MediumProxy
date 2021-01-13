8b2a928aedccce7ce659797a7709483cbc3665f4
cuongmx <cuongmx@gmail.com>
2021-01-13 21:55:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
 from flask import Flask,request,redirect,Response
-import requests
+import requests, os
 
 app = Flask('__main__')
-SITE_NAME = 'https://cuongmx.medium.com/'
-SITE_NEW = 'https://mediumx.herokuapp.com/'
+SITE_NAME = os.environ.get('SITE_NAME', 'https://cuongmx.medium.com/')
+SITE_NEW = os.environ.get('SITE_NEW', 'https://m.cuong.mx/')
 
 @app.route('/favicon.ico')
 #@app.route('/_/batch', methods=['POST'])
 def nothing():
-  """
-  docstring
-  """
   return "Nope"
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['POST','GET', 'OPTION'])
 def proxy(path):
-#  return get(f'{SITE_NAME}{path}', verify=False).content
   if request.method=='GET':
     resp = requests.get(f'{SITE_NAME}{path}')
-    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-    headers.append(['Access-Control-Allow-Origin', '*'])
-    response = Response(resp.content.decode('utf-8').replace(SITE_NAME, SITE_NEW), resp.status_code, headers)
-    return response
   elif request.method=='POST':
     resp = requests.post(f'{SITE_NAME}{path}',data=request.data)
-    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-    headers.append(['Access-Control-Allow-Origin', '*'])
-    response = Response(resp.content.decode('utf-8').replace(SITE_NAME, SITE_NEW), resp.status_code, headers)
-    return response
+  else: #'OPTION'
+    resp = requests.options(f'{SITE_NAME}{path}')
+  excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+  headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+  headers.append(['Access-Control-Allow-Origin', '*'])
+  response = Response(resp.content.decode('utf-8').replace(SITE_NAME, SITE_NEW), resp.status_code, headers)
+  return response
